fff8e8bc975552cdbefcf3a58c1423ac7a751860
Shaik Rahulpasha <rahul@xyz.com>
2024-06-06 13:52:13 +0530

    added hand of height solution

---
+++ Map/hand_of_height.cpp
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/hand-of-straights/
+
+class Solution {
+public:
+
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        std::map<int,int> freq;
+        for(int i=0;i<hand.size();i++)
+        {
+            freq[hand[i]]+=1;
+        }
+       
+        while(freq.size()!=0)
+        {
+            int key=freq.begin()->first;
+           for(int i=key;i<groupSize+key;i++)
+           {
+            std::cout<<i<<std::endl;
+            if(freq.count(i)==0)
+            {
+                return false;
+            }
+            freq[i]-=1;
+            
+            if(freq[i]==0)
+            {
+                freq.erase(i);
+            }
+           }
+            
+        }
+        
+        
+     return true;
+    }
+};
